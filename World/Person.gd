8c88a2b0f13c7cd83a9ae38a32167f7eaf037049
Torsten Wiederkehr <torsten.wiederkehr@gmail.com>
2024-06-28 08:58:29 +0200

    Added animation to persons you can talk to Added more story line
    Reviewed timelines

--- World/Person.gd
+++ World/Person.gd
@@ -2,8 +2,15 @@ class_name Person
 extends CharacterBody2D
 
 @export var timeline : DialogicTimeline
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 var can_talk = false
+
+
+func _ready():
+	var number_of_frames = animated_sprite_2d.sprite_frames.get_frame_count("default")
+	animated_sprite_2d.frame = randi_range(0, number_of_frames)
+	animated_sprite_2d.play("default")
 
 
 func _input(event):
