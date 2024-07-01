9053a1048689a18867c5b4128ebbdaf1a4891ac3
IN57KC <torsten.wiederkehr@ing.com>
2024-07-01 15:59:15 +0200

    Fixed Bug: Old mine quest was solved even be entering an leaving the old mine. Now it is solved by staring the battle against the necromancer

---
+++ World/Maps/NecromancerEncounter.gd
@@ -0,0 +1,14 @@
+class_name NecromancerEncounter
+extends Encounter
+
+
+func _change_scene():
+	Dialogic.VAR.old_mine = true
+	super._change_scene()
+
+
+
+
+
+
+
--- World/Maps/OldMineMap.gd
+++
@@ -1,6 +0,0 @@
-class_name OldMineMap
-extends Map
-
-
-func _on_necromancer_encounter_tree_exited():
-	Dialogic.VAR.old_mine = true
