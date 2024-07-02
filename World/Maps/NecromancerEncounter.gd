0cf362a5461563ed69a87dd8d45975da5e83e52e
IN57KC <torsten.wiederkehr@ing.com>
2024-07-02 14:02:27 +0200

    Added congratulation to old mine

--- World/Maps/NecromancerEncounter.gd
+++ World/Maps/NecromancerEncounter.gd
@@ -3,7 +3,7 @@ extends Encounter
 
 
 func _change_scene():
-	Dialogic.VAR.old_mine = true
+	Dialogic.VAR.OldMine.completed = true
 	super._change_scene()
 
 
--- World/Maps/OldMineMap.gd
+++ World/Maps/OldMineMap.gd
@@ -3,5 +3,7 @@ extends Map
 
 
 func _ready():
+	super._ready()
 	if Dialogic.VAR.OldMine.completed and not Dialogic.VAR.OldMine.congratulation:
+		Dialogic.VAR.OldMine.congratulation = true
 		Dialogic.start("congratulation")
